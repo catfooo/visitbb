84d72c4f75aea331d62d3a6ed1c92ff20ff88025
So Youn Choi <manghatuhyo@gmail.com>
2026-04-18 13:59:56 +0200

    example domain

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.use(async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
