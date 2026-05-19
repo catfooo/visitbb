574f7acbd64df5a3b8a4acf7eec384a13b2c63a0
So Youn Choi <manghatuhyo@gmail.com>
2026-05-19 18:26:47 +0200

    translation fix

--- server.js
+++ server.js
@@ -397,6 +397,17 @@ translated = translated.replace(/색안경/g, "선글라스");
 translated = translated.replace(/수하물 운반용 건조 가방/g, "짐 운반용 방수 가방");
 translated = translated.replace(/카메라를 담는 건조 가방/g, "카메라를 담는 방수 가방");
 translated = translated.replace(/가방 운송은 개방형 보트에 의해 수행되므로 편안함을 위해 딱딱한 시체가 없는 방수 가방에 수하물을 포장하는 것이 좋습니다. 필요한 경우 수하물을 추가 보호 범위로 포장하므로 필수는 아니지만 추가 보호 계층이 유용할 것입니다./g, "편안한 여행을 위해 방수 기능이 있는 소프트백(딱딱한 프레임이 없는 가방)에 짐을 챙기는 것을 강력히 권장합니다. 짐 운반은 개방형 보트를 통해 이루어지기 때문입니다. 필수 사항은 아니며, 필요 시 추가 방수 커버로 짐을 보호해 드리지만, 추가적인 방수 보호가 있으면 더욱 유용합니다.");
+translated = translated.replace(/이동 온도는 항상/g, "이동 템포는 항상");
+translated = translated.replace(/운송이 복잡할 수 있지만/g, "여행이 난이도가 있을 수 있지만");
+translated = translated.replace(/러시아스키/g, "루스키");
+translated = translated.replace(/GUESTHOUSE RUSSKIY DOM/g, "루스키 돔 게스트하우스");
+translated = translated.replace(/들판길의 도시락/g, "이동 도중에 제공되는 도시락");
+translated = translated.replace(/유당 프리/g, "락토스 프리");
+translated = translated.replace(/북극과 스발바르 지방의 다양한 속성 외에도/g, "북극과 스발바르 지방이라는 속성을 반영하는 다양한 상품들 외에도");
+translated = translated.replace(/노르웨이어와 러시아의/g, "노르웨이와 러시아의");
+translated = translated.replace(/기타 알코올 음료는 오후 1시까지만 /g, "기타 알코올 음료는 1시까지만 ");
+translated = translated.replace(/무선 침묵 및 드론 금지 구역/g, "전파 사용 제한(radio silence) 및 드론 금지 구역");
+translated = translated.replace(/찍는 규칙을 숙지하십시오/g, "촬영할 때 적용되는 규정을 숙지해야 합니다");
 // translated = translated.replace(/Grumant/g, "그루만트");
 
 
