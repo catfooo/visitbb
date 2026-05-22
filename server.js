28ac1aeb2a32e4ae895349a68f7545c15e2ad956
So Youn Choi <manghatuhyo@gmail.com>
2026-05-23 01:27:37 +0200

    ill kms

--- server.js
+++ server.js
@@ -727,7 +727,7 @@ app.listen(PORT, () => {
 if (process.env.NODE_ENV === "production") {
   setInterval(() => {
     // fetch("https://barentsburg.onrender.com")
-    fetch("visitbb.up.railway.app")
+    fetch("https://visitbb.up.railway.app")
       .then(() => console.log("keep-alive ping"))
       .catch(err => console.log("ping error:", err.message));
   }, 5 * 60 * 1000);
