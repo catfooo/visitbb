8a9dcb2184cb6e70add19b253d2b4b9b7524b4ac
So Youn Choi <manghatuhyo@gmail.com>
2026-05-23 03:05:18 +0200

    translation fix

--- server.js
+++ server.js
@@ -479,8 +479,10 @@ translated = translated.replace(/숙박소/g, "호스텔");
 translated = translated.replace(/바렌츠부르크에서; 에이/g, ", 피라미덴의 ");
 translated = translated.replace(/피라미덴\)/g, "을 소유하고 있습니다. ");
 translated = translated.replace(/에게/g, "으로부터");
-translated = translated.replace(/사파리, 관광 등\)/g, "사파리, 관광 등\) 스발바르에서 다양한 모험과 자연 체험을 제공합니다");
+//x
+translated = translated.replace(/사파리, 관광 등\)/g, "사파리, 관광 등) 스발바르에서 다양한 모험과 자연 체험을 제공합니다");
 translated = translated.replace(/스발바르에서 다양한 모험과 자연 체험을 제공합니다./g, "");
+translated = translated.replace(/보트, 하이킹, 카약, 겨울 및/g, ", 보트, 하이킹, 카약, 겨울 및 ");
 // translated = translated.replace(/Grumant/g, "그루만트");
 
 
