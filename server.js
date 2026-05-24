459809ad3e2a929b3c6448d6bf642a671456f428
So Youn Choi <manghatuhyo@gmail.com>
2026-05-24 02:50:34 +0200

    translation fix

--- server.js
+++ server.js
@@ -527,6 +527,9 @@ translated = translated.replace(/거리는 약 75km이며/g, "까지의 거리�
 translated = translated.replace(/하루는 저녁 식사로 마무리됩니다./g, "하루는 ");
 translated = translated.replace(/다음 모험을 시작하기 전에/g, "의 저녁 식사로 마무리됩니다. 다음 모험을 시작하기 전에");
 translated = translated.replace(/밤새:/g, "숙박:");
+translated = translated.replace(/호텔 조식 후 스노모빌 여행을 떠났습니다./g, "호텔 조식 후 ");
+translated = translated.replace(/, 넓은 바다를 향해 나아가다/g, "으로 스노모빌 여행을 떠나 ");
+translated = translated.replace(/. 우리의 경로는 얼어붙은 피요르드와/g, "의 넓은 바다를 향해 나아갑니다.");
 // translated = translated.replace(/Grumant/g, "그루만트");
 
 
