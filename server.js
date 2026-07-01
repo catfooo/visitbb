a3abef978f326598d805b6a6c2488cb279de6131
So Youn Choi <manghatuhyo@gmail.com>
2026-07-01 13:47:49 +0200

    revert

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use(async (req, res) => {
 
     await page.goto(targetUrl, {
       waitUntil: "networkidle2", 
-      timeout: 320000
+      timeout: 120000
     });
 
     console.log("Requested:", targetUrl);
